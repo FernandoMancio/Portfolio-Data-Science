f40087b1783e99b25a3fc6d3a02346642eac92a7
Fernando Mâncio <fmanciovieira@yahoo.com.br>
2024-02-23 09:31:52 -0300

    Add files via upload

--- dash_titanic.py
+++ dash_titanic.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+import openpyxl
+import warnings
+warnings.filterwarnings('ignore')
 
 ### page config
 st.set_page_config(page_title="Dashboard Titanic", page_icon= ":anchor:",
